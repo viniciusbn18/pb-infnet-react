1e261b9161dfa5ebab5e94ddc20cb2ade0dedae3
vinicius <vinicius.neves@al.infnet.edu.br>
2024-05-27 21:26:06 -0300

    Adicionando os tipos as props do componente

--- src/components/AvatarImage/index.tsx
+++ src/components/AvatarImage/index.tsx
@@ -1,6 +1,12 @@
 import { AvatarPictureContainer } from "./style";
 
-function AvatarImage({avatarSourceLink, avatarAltTitle, avatarTitle}) {
+interface AvatarImageProps {
+  avatarSourceLink: string;
+  avatarAltTitle: string;
+  avatarTitle: string;
+}
+
+const AvatarImage: React.FC<AvatarImageProps> = ({ avatarSourceLink, avatarAltTitle, avatarTitle }) => {
   return <AvatarPictureContainer src={avatarSourceLink} alt={avatarAltTitle} title={avatarTitle}/>
 }
 
